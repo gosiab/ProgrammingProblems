5574acf9b6066f3968501587a5c39c80c862fcd6
Gosia Borzęcka <gosia.borzecka@gmail.com>
2014-10-06 15:50:20 +0200

    Challenege: FrogJump

---
+++ Codility/CodilitySource/01.TimeComplexity/FrogJump.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CodilitySource
+{
+    public class FrogJump
+    {
+        public int Jump(int x, int y, int d)
+        {
+            if (x < 0 || y < 0 || d < 0
+                || x > 1000000000 || y > 1000000000
+                || d > 1000000000 || x > y)
+            {
+                return -1;
+            }
+
+            return (int)Math.Ceiling(((y - x) / (d * 1.0M)));
+        }
+    }
+}
+
+/**************************************************************
+ * A small frog wants to get to the other side of the road. The frog is currently located at position X and wants to get to a position greater than or equal to Y. The small frog always jumps a fixed distance, D.
+Count the minimal number of jumps that the small frog must perform to reach its target.
+Write a function:
+	int solution(int X, int Y, int D);
+that, given three integers X, Y and D, returns the minimal number of jumps from position X to a position equal to or greater than Y.
+For example, given:
+  X = 10
+  Y = 85
+  D = 30
+the function should return 3, because the frog will be positioned as follows:
+	• after the first jump, at position 10 + 30 = 40
+	• after the second jump, at position 10 + 30 + 30 = 70
+	• after the third jump, at position 10 + 30 + 30 + 30 = 100
+Assume that:
+	• X, Y and D are integers within the range [1..1,000,000,000];
+	• X ≤ Y.
+Complexity:
+	• expected worst-case time complexity is O(1);
+	• expected worst-case space complexity is O(1).
+*********************************************************************/
---
+++ Codility/CodilityTests/01.TimeComplexity/ForgJumpTests.cs
@@ -0,0 +1,135 @@
+﻿using CodilitySource;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodilityTests
+{
+    [TestClass]
+    public class ForgJumpTests
+    {
+        [TestMethod]
+        public void FrogJump_ExampleTest_Return3()
+        {
+            // Arrange
+            const int x = 10;
+            const int y = 85;
+            const int d = 30;
+            const int expected = 3;
+
+            var frogJump = new FrogJump();
+
+            // Act
+            var result = frogJump.Jump(x, y, d);
+
+            // Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void FrogJump_NegativeJump_ReturnMinus1()
+        {
+            // Arrange
+            const int x = -10;
+            const int y = 85;
+            const int d = 30;
+            const int expected = -1;
+
+            var frogJump = new FrogJump();
+
+            // Act
+            var result = frogJump.Jump(x, y, d);
+
+            // Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void FrogJump_NoJumpNedded_Return0()
+        {
+            // Arrange
+            const int x = 1;
+            const int y = 1;
+            const int d = 1;
+            const int expected = 0;
+
+            var frogJump = new FrogJump();
+
+            // Act
+            var result = frogJump.Jump(x, y, d);
+
+            // Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void FrogJump_ManyJumps2Disctance_Return499999995()
+        {
+            // Arrange
+            const int x = 10;
+            const int y = 1000000000;
+            const int d = 2;
+            const int expected = 499999995;
+
+            var frogJump = new FrogJump();
+
+            // Act
+            var result = frogJump.Jump(x, y, d);
+
+            // Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void FrogJump_ManyJumps99Distance_Return10101010()
+        {
+            // Arrange
+            const int x = 10;
+            const int y = 1000000000;
+            const int d = 99;
+            const int expected = 10101010;
+
+            var frogJump = new FrogJump();
+
+            // Act
+            var result = frogJump.Jump(x, y, d);
+
+            // Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void FrogJump_ManyJumps1283Distance_Return779424()
+        {
+            // Arrange
+            const int x = 10;
+            const int y = 1000000000;
+            const int d = 1283;
+            const int expected = 779424;
+
+            var frogJump = new FrogJump();
+
+            // Act
+            var result = frogJump.Jump(x, y, d);
+
+            // Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void FrogJump_OneBigJump_Return1()
+        {
+            // Arrange
+            const int x = 10;
+            const int y = 1000000000;
+            const int d = 1000000000;
+            const int expected = 1;
+
+            var frogJump = new FrogJump();
+
+            // Act
+            var result = frogJump.Jump(x, y, d);
+
+            // Assert
+            Assert.AreEqual(result, expected);
+        }
+    }
+}
